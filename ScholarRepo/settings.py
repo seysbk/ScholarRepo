0b0ae7e7b7fff892d4c6ce4118511072b8c04daf
seysbk <sbkuvuordo@gmail.com>
2025-10-28 14:37:35 +0000

    deployment

--- ScholarRepo/settings.py
+++ ScholarRepo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-b67ey3@%9e!26(#8w7!$)v$v16i%2t99k57k6=iua8mv&vqv=4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://scholarrepo.onrender.com']
 
 
 # Application definition
