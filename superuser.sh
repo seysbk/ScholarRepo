b66f61f82aa45e471bde46b4c30f6b5739f1f4e3
seysbk <sbkuvuordo@gmail.com>
2025-10-29 07:26:39 +0000

    superuser

--- superuser.sh
+++ superuser.sh
@@ -9,12 +9,3 @@ User = get_user_model()
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-if username and email and password:
-    if not User.objects.filter(username=username).exists():
-        print("Creating superuser...")
-        User.objects.create_superuser(username=username, email=email, password=password)
-    else:
-        print("Superuser already exists.")
-else:
-    print("Missing one or more environment variables for superuser.")
