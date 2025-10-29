1c73951f4157eed3c9827c2d2891b3ba190bd6f0
seysbk <sbkuvuordo@gmail.com>
2025-10-29 05:55:34 +0000

    middleware update

--- ScholarRepo/settings.py
+++ ScholarRepo/settings.py
@@ -55,12 +55,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
-MIDDLEWARE = [
-       'whitenoise.middleware.WhiteNoiseMiddleware',
-       ...
-   ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
