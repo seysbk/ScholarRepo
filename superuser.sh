9fade865f774a7526dd284b33cb28ced93b1b2af
seysbk <sbkuvuordo@gmail.com>
2025-10-29 04:48:00 +0000

    superuser.sh update

--- superuser.sh
+++ superuser.sh
@@ -1,11 +1,10 @@
- bash
-   #!/bin/bash
+#!/bin/bash
 
-   echo "from django.contrib.auth import get_user_model;
-   User = get_user_model();
-   username = 'DJANGO_SUPERUSER_USERNAME';
-   email = '{DJANGO_SUPERUSER_EMAIL}';
-   password = '${DJANGO_SUPERUSER_PASSWORD}';
-   if not User.objects.filter(username=username).exists():
-       User.objects.create_superuser(username, email, password)
-   " | python manage.py shell
+echo "from django.contrib.auth import get_user_model
+User = get_user_model()
+username = '{DJANGO_SUPERUSER_USERNAME}'
+email = 'DJANGO_SUPERUSER_EMAIL'
+password = '{DJANGO_SUPERUSER_PASSWORD}'
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+" | python manage.py shell
