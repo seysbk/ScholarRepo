4b9475da463bda52e543c208ec8accc19c53462b
seysbk <sbkuvuordo@gmail.com>
2025-10-29 06:50:01 +0000

    superuser update

--- superuser.sh
+++ superuser.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-echo "from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+import os
+
 User = get_user_model()
-username = '{DJANGO_SUPERUSER_USERNAME}'
-email = 'DJANGO_SUPERUSER_EMAIL'
-password = '{DJANGO_SUPERUSER_PASSWORD}'
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-" | python manage.py shell
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        print("Creating superuser...")
+        User.objects.create_superuser(username=username, email=email, password=password)
+    else:
+        print("Superuser already exists.")
+else:
+    print("Missing one or more environment variables for superuser.")
