16cd8dad83282f7a55a027e87cc695c067238288
seysbk <sbkuvuordo@gmail.com>
2025-10-31 14:23:09 +0000

    superuser

--- ScholarRepo/settings.py
+++ ScholarRepo/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-b67ey3@%9e!26(#8w7!$)v$v16i%2t99k57k6=iua8mv&vqv=4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['scholarrepo.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['scholarrepo.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- superuser.py
+++ superuser.py
@@ -1,11 +1,19 @@
-#!/bin/bash
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ScholarRepo.settings")
+django.setup()
 
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand
-import os
 
 User = get_user_model()
 
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
